e97e6fa4667245a5cac0181bae4718c7f5f43656
Juan Jose Duque Cardona <47718782+jjduquec@users.noreply.github.com>
2024-04-30 22:16:41 -0500

    first prototype of policy list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import  Policy from './Components/list_policy/index' 
+import './bootstrap/css/bootstrap.css';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( 
+    <div class="row"> 
+        
+    <div class="col col-6 col-md-4" >
+
+      <div class="card" style={{width: "18rem;"}}>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+
+      </div>
+      <div class="card" style={{width: "18rem;"}}>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+
+      </div>
+      <div class="card" style={{width: "18rem;"}}>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+
+      </div>
     </div>
+    <div class="col col-sm-6 col-md-8">
+      Second Column
+    </div>
+  </div>
+
+    
+    
+      
+        
+       
+      
+  
+  
+  
   );
 }
 
---
+++ src/Components/list_policy/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import './index.css'
+
+
+
+function Policy(){
+
+    return
+
+    
+        <div>
+            <p>Policy paragraph</p>
+        </div>
+
+
+
+
+    
+
+
+
+
+}
+
+
+
+
+/*
+class Policy extends React.Component{
+
+    render(){
+
+        return 
+            <div>
+                    <p>Policy paragraph</p>
+            </div>
+    }
+
+
+
+}
+
+*/
+
+
+export default Policy;
