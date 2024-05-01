6cca2916b01b53b313dbefa859ba83c91717f4c6
Juan Jose Duque Cardona <47718782+jjduquec@users.noreply.github.com>
2024-05-01 01:03:40 -0500

    updated background and border of cards

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './bootstrap/css/bootstrap.css';
 
 function App() {
   let policies_list=[
-    {customerId:'510-558-2282',Status:'Active',StartDate:'25/01/24',EndDate:'25/01/25'},
+    {customerId:'510-558-2282',Status:'Cancelled',StartDate:'25/01/24',EndDate:'25/01/25'},
     {customerId:'510-558-2283',Status:'Active',StartDate:'25/01/24',EndDate:'25/01/25'},
     {customerId:'510-558-2284',Status:'Active',StartDate:'25/01/24',EndDate:'25/01/25'},
     {customerId:'510-558-2285',Status:'Active',StartDate:'25/01/24',EndDate:'25/01/25'},
@@ -18,14 +18,16 @@ function App() {
 ]
   return ( 
     <div class="row"> 
-     <div class="col col-6 col-md-4" > 
-            {policies_list.map((temp_policy)=>(
-                <Policy data_policy={temp_policy}/>
+     <div class="col col-6 col-md-4" >
+        <div class="scrollable-column" > 
+              {policies_list.map((temp_policy)=>(
+                  <Policy data_policy={temp_policy}/>
 
-                ))  
-      
-      
-            }
+                  ))  
+        
+        
+              }
+          </div>
       </div>
     <div class="col col-sm-6 col-md-8">
       Second Column
--- src/Components/policy/index.js
+++ src/Components/policy/index.js
@@ -9,7 +9,7 @@ function Policy ({data_policy}){
         <div class="card " style={{width: "18rem"}}>
             <div class="card-body">
             <h5 class="card-title">Customer ID: {data_policy.customerId} </h5>
-            <p class="card-text">Status: {data_policy.Status}</p>
+            <p class="card-text" >Status: {data_policy.Status}</p>
             <p class="card-text">Start Date : {data_policy.StartDate}</p>
             <p class="card-text">End Date : {data_policy.EndDate}</p>
             
