73084919f90cf978a78735033851f154321c4d34
Juan Jose Duque Cardona <47718782+jjduquec@users.noreply.github.com>
2024-05-01 00:47:25 -0500

    cards style updated

--- src/App.js
+++ src/App.js
@@ -1,38 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import  Policy from './Components/list_policy/index' 
+
+import Policy from './Components/policy/index.js';
 import './bootstrap/css/bootstrap.css';
 
 
 
 function App() {
+  let policies_list=[
+    {customerId:'510-558-2282',Status:'Active',StartDate:'25/01/24',EndDate:'25/01/25'},
+    {customerId:'510-558-2283',Status:'Active',StartDate:'25/01/24',EndDate:'25/01/25'},
+    {customerId:'510-558-2284',Status:'Active',StartDate:'25/01/24',EndDate:'25/01/25'},
+    {customerId:'510-558-2285',Status:'Active',StartDate:'25/01/24',EndDate:'25/01/25'},
+    {customerId:'510-558-2286',Status:'Active',StartDate:'25/01/24',EndDate:'25/01/25'},
+    {customerId:'510-558-2287',Status:'Active',StartDate:'25/01/24',EndDate:'25/01/25'}
+    
+]
   return ( 
     <div class="row"> 
-        
-    <div class="col col-6 col-md-4" >
+     <div class="col col-6 col-md-4" > 
+            {policies_list.map((temp_policy)=>(
+                <Policy data_policy={temp_policy}/>
 
-      <div class="card" style={{width: "18rem;"}}>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-
+                ))  
+      
+      
+            }
       </div>
-      <div class="card" style={{width: "18rem;"}}>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-
-      </div>
-      <div class="card" style={{width: "18rem;"}}>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-
-      </div>
-    </div>
     <div class="col col-sm-6 col-md-8">
       Second Column
     </div>
--- src/Components/policy/index.js
+++ src/Components/policy/index.js
@@ -6,7 +6,7 @@ function Policy ({data_policy}){
 
     return(
 
-        <div class="card" style={{width: "18rem"}}>
+        <div class="card " style={{width: "18rem"}}>
             <div class="card-body">
             <h5 class="card-title">Customer ID: {data_policy.customerId} </h5>
             <p class="card-text">Status: {data_policy.Status}</p>
