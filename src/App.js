047ab63aa52b97b9a97db2b821ef0681f26468ad
Juan Jose Duque Cardona <47718782+jjduquec@users.noreply.github.com>
2024-05-01 17:31:06 -0500

    table implementation at column 2th

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import axios from 'axios';
 function App() {
 
   const[policies_list,setList]=useState([])
-
+/*
   useEffect(()=>{
     axios({
       url: "http://localhost:3001/policies/api/v1/policies",
@@ -38,28 +38,56 @@ function App() {
       },[setList]);
 
   });
-  //let policies_list=[{customerId:'510-558-2282',Status:'Cancelled',StartDate:'25/01/24',EndDate:'25/01/25'}];
+  */
+
+  /*delete this before update main */  
+  let policies=[
+    {customerId:'510-558-2282',Status:'Cancelled',StartDate:'25/01/24',EndDate:'25/01/25'},
+    {customerId:'510-558-2283',Status:'Active',StartDate:'25/01/24',EndDate:'25/01/25'},
+    {customerId:'510-558-2284',Status:'Active',StartDate:'25/01/24',EndDate:'25/01/25'},
+    {customerId:'510-558-2285',Status:'Active',StartDate:'25/01/24',EndDate:'25/01/25'}
+  ]
+
 
  
   return ( 
     
-    <div class="row"> 
-     <div class="col col-6 col-md-4" >
-        
-              
-              {policies_list.map((temp_policy)=>(
-                  <Policy key={temp_policy.customerId} data_policy={temp_policy}/>
-
-                  ))  
-        
-        
-              }
+    <div class="row justify-content-start"> 
+      <div class="col col-6 col-md-4" >
           
-      </div>
-    <div class="col col-lg-">
-      Second Column
+                
+                {policies.map((temp_policy)=>(
+                    <Policy key={temp_policy.customerId} data_policy={temp_policy}/>
+
+                    ))  
+          
+          
+                }
+            
+        </div>
+        <div class="col col-5">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Transaction Id</th>
+                <th scope="col">Operation Type</th>
+                <th scope="col">Process Date</th>
+                <th scope="col">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                  <td>8f2e05ef-0e61-4f47-8b0b-157fea83e7d8</td>
+                  <td>ISS</td>
+                  <td>2024-04-26</td>
+                  <td>ACTIVE</td>
+              </tr>
+
+            </tbody>
+          </table>
+        </div>
+      
     </div>
-  </div>
 
     
     
