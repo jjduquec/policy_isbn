5713ec6d04bc2b3d1053e362248a7c125c81f63d
Juan Jose Duque Cardona <47718782+jjduquec@users.noreply.github.com>
2024-05-01 00:23:05 -0500

    listing policiies

---
+++ src/Components/policy/index.js
@@ -0,0 +1,27 @@
+import './index.css'
+function Policy ({data_policy}){
+
+
+    
+
+    return(
+
+        <div class="card" style={{width: "18rem"}}>
+            <div class="card-body">
+            <h5 class="card-title">Customer ID: {data_policy.customerId} </h5>
+            <p class="card-text">Status: {data_policy.Status}</p>
+            <p class="card-text">Start Date : {data_policy.StartDate}</p>
+            <p class="card-text">End Date : {data_policy.EndDate}</p>
+            
+            </div>
+        </div>
+
+    );
+
+
+    
+
+
+}
+
+export default Policy;
